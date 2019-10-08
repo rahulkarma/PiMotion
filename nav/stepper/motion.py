b39678a8cc6770fd5b8970cd071a134b2c51e531
Rahul Vishwakarma <karmarahul369@gmail.com>
2019-10-08 00:09:15 -0500

    Implemented strafe motion

--- nav/stepper/motion.py
+++ nav/stepper/motion.py
@@ -2,7 +2,7 @@ from time import sleep
 from .stepper_constants import *
 
 class StepperMotion:
-	def __init__(self, GPIO):
+    def __init__(self, GPIO):
             self.step_mode_select = [STEPPER_m0, STEPPER_m1, STEPPER_m2]
             self.wheels_step = [BR_step, FR_step, BL_step, FL_step]
             self.wheels_dir = [BR_dir, FR_dir, BL_dir, FL_dir]
@@ -10,51 +10,68 @@ class StepperMotion:
             
             [GPIO.setup(pin, GPIO.OUT) for pin in out_pins]
 
-	"""
-	TODO: Move forward and back
+    """
+    TODO: Move forward and back
 
-	Change Log
-		[0.0.0] Rishav
-			--- Created function
-	"""
-	def mov(self, dir, distance, GPIO):
-            for pin in self.step_mode_select:
-                GPIO.output(pin, GPIO.HIGH)
-                
-            distance = int(distance* STEPS_PER_INCH)
-            sleep(1)
-                
-            if(dir == 0):
-                for pin in [BR_dir, FR_dir]:
-                    GPIO.output(pin, CCW)
-                for pin in [BL_dir, FL_dir]:
-                    GPIO.output(pin, CW)
+    Change Log
+        [0.0.0] Rishav
+            --- Created function
+    """
+    def mov(self, dir, distance, GPIO):
+        
+        distance = int(distance * STEPS_PER_INCH)
+        
+        sleep(1)
+        if(dir == 0):
+            for pin in [BR_dir, FR_dir]:
+                GPIO.output(pin, CCW)
+            for pin in [BL_dir, FL_dir]:
+                GPIO.output(pin, CW)
                     
-            if(dir == 1):
-                for pin in [BR_dir, FR_dir]:
-                    GPIO.output(pin, CW)
-                for pin in [BL_dir, FL_dir]:
-                    GPIO.output(pin, CCW)
+        if(dir == 1):
+            for pin in [BR_dir, FR_dir]:
+                GPIO.output(pin, CW)
+            for pin in [BL_dir, FL_dir]:
+                GPIO.output(pin, CCW)
 
-                for _ in range(distance):
-                    [GPIO.output(x, GPIO.HIGH) for x in self.wheels_step]
-                    sleep(STEPPER_DELAY)
-                    [GPIO.output(x, GPIO.LOW) for x in self.wheels_step]
-                    sleep(STEPPER_DELAY)
-                
-                sleep(.5)
-                
-                for pin in self.step_mode_select:
-                    GPIO.output(pin, GPIO.LOW)
+        for _ in range(distance):
+            [GPIO.output(x, GPIO.HIGH) for x in self.wheels_step]
+            sleep(STEPPER_DELAY)
+            [GPIO.output(x, GPIO.LOW) for x in self.wheels_step]
+            sleep(STEPPER_DELAY)
+        
+    # TODO: Turn left and right
+    def turn(self, dir, degrees):
+        pass
 
-	# TODO: Turn left and right
-	def turn(self, dir, degrees):
-		pass
+    """
+    For strafe right motion, FR=BL=fwd and BR=FL=rev
+    For strafe left motion, FL=BR=fwd and FR=BL=rev
 
-	# TODO: Strafe left and right
-	def strafe(self, dir, distance):
-		pass
+    Change Log
+        [0.0.0] Rishav
+            --- Created function
+        [0.0.1] Rahul
+            --- Added GPIO as input for motor movement
+            --- Implemented left and right strafe movement
+    """
+    def strafe(self, dir, distance, GPIO):
+        #if true, strafe right
+        if dir:
+            [GPIO.output(pin, CW) for pin in [FL_dir, FR_dir]]
+            [GPIO.output(pin, CCW) for pin in [BR_dir, BL_dir]]
+        #else, strafe left
+        else:
+            [GPIO.output(pin, CW) for pin in [BL_dir, BR_dir]]
+            [GPIO.output(pin, CCW) for pin in [FR_dir, FL_dir]]
+         #runs the motor for the given distance in inch   
+        for _ in range(int(STEPS_PER_INCH_STRAFE * distance)):
+            [GPIO.output(pin, GPIO.HIGH) for pin in self.wheels_step]
+            sleep(STEPPER_DELAY)
+            [GPIO.output(pin, GPIO.LOW) for pin in self.wheels_step]
+            sleep(STEPPER_DELAY)
+        
 
-	# TODO: Diagonal movement
-	def diagonal_move(self, dir, distance):
-		pass
+    # TODO: Diagonal movement
+    def diagonal_move(self, dir, distance):
+        pass
--- nav/stepper/stepper_constants.py
+++ nav/stepper/stepper_constants.py
@@ -4,6 +4,8 @@ STEPPER_m0 = 17
 STEPPER_m1 = 27
 STEPPER_m2 = 22
 
+STEP_MODE_SELECT = [STEPPER_m0, STEPPER_m1, STEPPER_m2]
+
 # First wheel GPIO Pin
 BR_step = 21
 BR_dir = 20
@@ -20,9 +22,10 @@ BL_dir = 8
 FL_step = 24
 FL_dir = 23
 
-CW = 1 			                 # Clockwise rotation
-CCW = 0 		                 # Counter-clockwise rotation
-STEPS_PER_REVOLUTION = 800 		 # Steps per Revolution (360/1.8) * 4. Multiply by 4 because quarter step.
+CW = 1                           # Clockwise rotation
+CCW = 0                          # Counter-clockwise rotation
+STEPS_PER_REVOLUTION = 800       # Steps per Revolution (360/1.8) * 4. Multiply by 4 because quarter step.
 DISTANCE = 60*math.pi/25.4
 STEPS_PER_INCH = STEPS_PER_REVOLUTION/DISTANCE
 STEPPER_DELAY = 0.0005
+STEPS_PER_INCH_STRAFE = STEPS_PER_REVOLUTION/(DISTANCE*0.9)  # Calibrates steps_per_inch for strafe motion.
